8466458164ad593edb5aceec9f07b5e0a27535ce
asadbek <asadkasal923@gmail.com>
2025-08-30 15:09:48 +0500

    c

--- apps/admin.py
+++ apps/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from apps.models import Galaxy, Planet, Group, Star, Gallery, Exploration, About, Holes_Matter, Register, \
+from apps.models import Galaxy, Planet, Group, Star, Gallery, Exploration, About, Holes_Matter, Register, Faktlar, \
     Fakt_turi
 
 # from apps.models import Product, Category
@@ -16,23 +16,7 @@ admin.site.register(Exploration)
 admin.site.register(About)
 admin.site.register(Holes_Matter)
 admin.site.register(Register)
-
-from django.contrib import admin
-from .models import Fakt_turi, Faktlar
-
-
-class FaktlarInline(admin.TabularInline):
-    model = Faktlar
-    extra = 1
+admin.site.register(Faktlar)
+admin.site.register(Fakt_turi)
 
 
-@admin.register(Fakt_turi)
-class FaktTuriAdmin(admin.ModelAdmin):
-    list_display = ('title',)
-    inlines = [FaktlarInline]
-
-
-@admin.register(Faktlar)
-class FaktlarAdmin(admin.ModelAdmin):
-    list_display = ('title', 'fakt')
-    list_filter = ('fakt',)
